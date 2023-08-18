6a72b72d33b1b862ca95f17dc33ff0c41738da28
Rameez <rameesmohd789@gmail.com>
2023-08-18 21:57:26 +0530

    started working on forget passsword with otp

--- src/Components/User/Login.jsx
+++ src/Components/User/Login.jsx
@@ -1,12 +1,17 @@
 import React, { useRef, useState } from 'react'
 
 import userAxios from '../../Axios/UserAxios'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import {CgSpinner} from 'react-icons/cg'
+import { toast } from 'react-toastify'
 
 const Login = () => {
     const emailRef = useRef()
     const passwordRef = useRef()
     const [error ,setError] = useState({})
+    const [forgot,setForgot] = useState(false)
+    const [userExist,setUser] =useState(true)
+    const [loading,setLoading] = useState(false)
     const navigate = useNavigate()
     
     const handleSubmit=(event)=>{
@@ -33,28 +38,83 @@ const Login = () => {
             setError(validationErrors)
         }
     }
+    
+    const forgotPass =async(email)=>{
+      
+        if(!email){
+            toast.error('Email is required');
+        }else if(!/\S+@\S+\.\S+/.test(email)){
+            toast.error('Invalid email format');
+        }else{
+            setLoading(true)
+            await userAxios.post('/forgetpassword',{email}).then((res)=>{
+                if(res.status){
+                    setUser(true)
+                }
+            })
+        }
+    }
 
     return (
-            <div className='bg-grey-100 flex flex-col justify-center'>
+        <div className='bg-grey-100 flex flex-col justify-center'>
+            { !forgot ? 
             <form onSubmit={handleSubmit} className='max-w-[400px] w-full mx-auto bg-white p-4'>
                 <h2 className='text-4xl font-bold text-center py-6'>Sign In</h2>
                 <div className='flex flex-col py-2'>
                     <label>Username</label>
-                    <input ref={emailRef} type='text' className='border p-2' placeholder='enter your email'/>
+                    <input ref={emailRef} type='text' className='border p-2' placeholder='Enter your email'/>
                     {error.email && <div className="error text-red-700">{error.email}</div>}
                 </div>
                 <div className='flex flex-col py-2'>
                     <label>Password</label>
-                    <input ref={passwordRef} type='text' className='border p-2' placeholder='password'/>
+                    <input ref={passwordRef} type='text' className='border p-2' placeholder='Password'/>
                     {error.password && <div className="error text-red-700">{error.password}</div>}
                 </div>
                 <button type='submit' className='text-white border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-400'>Login</button>
                 {error.resError && <div className="error text-red-700">{error.resError}</div>}
                 <div className='flex justify-between '>
                     <p className='flex items-center'><input className='mr-2' type="checkbox"/>Remember Me</p>
-                    <p>Create an account</p>
+                    <Link to={'/signup'}>Create an account</Link>
                 </div>
-            </form>
+                <Link onClick={()=>setForgot(true)}>Forget password</Link>
+            </form> :( !userExist ?
+            <form onSubmit={handleSubmit} className='max-w-[400px] w-full mx-auto bg-white p-4 border '>
+                <h2 className='text-4xl font-bold text-center py-6'>Forget Password</h2>
+                <div className='flex flex-col py-2'>
+                    <input ref={emailRef} type='text' className='border p-2' placeholder='Enter your email'/>
+                    <button type='button' onClick={()=>forgotPass(emailRef.current.value)} className='text-white border
+                     w-full my-8 py-2 bg-indigo-600 hover:bg-indigo-400 flex justify-center'>
+                    { 
+                        loading && <CgSpinner size={20} className='mt-1 animate-spin mr-2' />
+                    }
+                        <span>Submit</span>
+                    </button>
+                    <Link onClick={()=>setForgot(false)} className='text-right'>Back to login</Link>
+                </div>
+            </form> : 
+            <form onSubmit={handleSubmit} className='max-w-[400px] w-full mx-auto bg-white p-4 border '>
+                 <h2 className='text-4xl font-bold text-center py-6'>Forget Password</h2>
+                <div className='flex flex-col py-2'>
+                    <div className='flex flex-col py-2'>
+                    <label>Password</label>
+                    <input className='border p-2' required/>
+                    {error.password && <div className="error text-red-700">{error.password}</div>}
+                    </div>
+                    <div className='flex flex-col py-2'>
+                    <label>Confirm Password</label>
+                    <input type='password' className='border p-2'  required/>
+                    {error.nomatch && <div className="error text-red-700">{error.nomatch}</div>}
+                    </div>
+                    <button type='button' onClick={()=>forgotPass(emailRef.current.value)} className='text-white border
+                     w-full my-8 py-2 bg-indigo-600 hover:bg-indigo-400 flex justify-center'>
+                    { 
+                    loading && <CgSpinner size={20} className='mt-1 animate-spin mr-2' />
+                    }
+                    <span>Submit</span>
+                    </button>
+                    <Link onClick={()=>setForgot(false)} className='text-right'>Back to login</Link>
+                </div>
+            </form> )}
         </div>
   )
 }
