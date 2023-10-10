3438651ac818a5a9244ec6344e38bb807f874e23
Rameez <rameesmohd789@gmail.com>
2023-10-10 11:12:57 +0530

    error4 solved

--- src/Pages/User/Market.jsx
+++ src/Pages/User/Market.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from '../../Components/User/Navbar'
 import Footer from '../../Components/User/Footer'
-import Body from '../../Components/User/Markets/MainBody'
+import MainBody from '../../Components/User/Markets/MainBody'
 
 const Market = () => {
   return (
     <div className='lg:container mx-auto'>
         <Navbar/>
-        <Body/>
+        <MainBody/>
         <Footer/>
     </div>
   )
