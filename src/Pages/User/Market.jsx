6ca0b5be241a51b4a089585f636642ad82bb957c
Rameez <rameesmohd789@gmail.com>
2023-10-10 11:19:07 +0530

    error4

--- src/Pages/User/Market.jsx
+++ src/Pages/User/Market.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../Components/User/Navbar'
 import Footer from '../../Components/User/Footer'
-import MainBody from '../../Components/User/Markets/MainBody'
+import MainBody from './../../Components/User/Markets/MainBody'
 
 const Market = () => {
   return (
