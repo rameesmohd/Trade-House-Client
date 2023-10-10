e85d9f4b52a08a322720f1e19a65b23853bb37a5
Rameez <rameesmohd789@gmail.com>
2023-10-10 13:01:05 +0530

    error5

--- src/Components/Chat/ChatRoom.jsx
+++ src/Components/Chat/ChatRoom.jsx
@@ -39,7 +39,7 @@ function ChatComponent({role}) {
   }
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5001")
+    const newSocket = io("https://api.tradeh.online/")
     setSocket(newSocket)
     newSocket.on("error",(err)=>{
       console.log(err);
