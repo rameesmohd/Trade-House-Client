c7a283289fe0009377516ca9e35e2d0632ebe313
Rameez <rameesmohd789@gmail.com>
2023-10-10 12:08:21 +0530

    error5

--- src/Pages/UserPage/Market.jsx
+++ src/Pages/UserPage/Market.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from '../../Components/User/Navbar'
 import Footer from '../../Components/User/Footer'
-// import MainBody from '../../Components/User/Markets/MainBody'
+import MainBody from '../../Components/User/ShowMarkets/MainBody'
 
 const Market = () => {
   return (
     <div className='lg:container mx-auto'>
         <Navbar/>
-        {/* <MainBody/> */}
+        <MainBody/>
         <Footer/>
     </div>
   )
