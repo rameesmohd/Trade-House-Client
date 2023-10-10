f05e470d18891605a242c08b72640b741b127063
Rameez <rameesmohd789@gmail.com>
2023-10-10 12:56:25 +0530

    error5

--- src/Constants/API.js
+++ src/Constants/API.js
@@ -1,5 +1,10 @@
-export const userAPI ="http://localhost:5001/";
-export const adminAPI ="http://localhost:5001/admin/";
-export const tutorAPI ="http://localhost:5001/tutor/";
+// export const userAPI ="http://localhost:5001/";
+// export const adminAPI ="http://localhost:5001/admin/";
+// export const tutorAPI ="http://localhost:5001/tutor/";
+
+export const userAPI ="https://api.tradeh.online/";
+export const adminAPI ="https://api.tradeh.online/admin/";
+export const tutorAPI ="https://api.tradeh.online/tutor/";
+
 export const GoogleClientId ='873744942590-1pmttm90g91l7v65g5illcthig1jpcem.apps.googleusercontent.com'
 
